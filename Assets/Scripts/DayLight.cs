cd8715ea1b84a1964416620de9ec9067d83d4308
Chi Shen <shenchi710@gmail.com>
2016-10-12 19:25:32 -0400

    day light

---
+++ Assets/Scripts/DayLight.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DayLight : MonoBehaviour
+{
+    public Color tintColor = Color.white;
+
+    private Material mat;
+
+    void Awake()
+    {
+        mat = new Material(Shader.Find("Hidden/WholeScreenTint"));
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        mat.color = tintColor;
+        Graphics.Blit(src, dest, mat);
+    }
+}
---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeManager : MonoBehaviour
+{
+    public Transform clockPointer;
+    public DayLight dayLightEffect;
+    public float timeForDayTime = 90.0f;
+
+    public Color dawnColor;
+    public Color noonColor;
+
+    private float timeWhenDayBegin;
+    
+    /// <summary>
+    /// 0 for 8:00 am
+    /// </summary>
+    public float VirtualTimeInHour { get; private set; }
+
+    // Use this for initialization
+    void Start()
+    {
+        timeWhenDayBegin = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float elapsedTime = Time.time - timeWhenDayBegin;
+        float progress = elapsedTime / timeForDayTime;
+        VirtualTimeInHour = progress * 12;
+
+        if (null != clockPointer)
+        {
+            float angle = progress * 360.0f - 120.0f;
+            clockPointer.localRotation = Quaternion.Euler(0, 0, -angle);
+        }
+
+        if (null != dayLightEffect)
+        {
+            float t = Mathf.Clamp01(Mathf.Pow((Mathf.Cos(progress * 2.0f * Mathf.PI) + 1) * 0.5f, 7) );
+            dayLightEffect.tintColor = Color.Lerp(noonColor, dawnColor, t);
+        }
+    }
+}
