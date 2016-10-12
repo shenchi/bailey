7a9b7a0d4d3b0e6ad5f20811c02a2a0422783aa5
Chi Shen <shenchi710@gmail.com>
2016-10-11 20:26:04 -0400

    camera follower & basic player control

---
+++ Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollower : MonoBehaviour
+{
+    public GameObject followee;
+    public Tiled2Unity.TiledMap map;
+
+    public float moveThreshold = 0.01f;
+    public float followSpeed = 10.0f;
+
+    private Camera cam;
+
+    private Rect mapArea;
+
+    private float halfViewSizeX;
+    private float halfViewSizeY;
+
+    void OnEnable()
+    {
+        cam = GetComponent<Camera>();
+        halfViewSizeY = cam.orthographicSize;
+        halfViewSizeX = cam.orthographicSize * cam.aspect;
+
+        mapArea.x = map.transform.position.x;
+        mapArea.y = map.transform.position.y - map.MapHeightInPixels;
+        mapArea.width = map.MapWidthInPixels;
+        mapArea.height = map.MapHeightInPixels;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float targetX = followee.transform.position.x;
+        float targetY = followee.transform.position.y;
+        float deltaX = targetX - transform.position.x;
+        float deltaY = targetY - transform.position.y;
+
+        if (Mathf.Abs(deltaX) < moveThreshold &&
+            Mathf.Abs(deltaY) < moveThreshold)
+        {
+            return;
+        }
+
+        //float speedX = Mathf.Min(deltaX, followSpeed);
+        //float speedY = Mathf.Min(deltaY, followSpeed);
+
+        //float newX = transform.position.x + speedX * Time.fixedDeltaTime;
+        //float newY = transform.position.y + speedY * Time.fixedDeltaTime;
+
+        //float newX = Mathf.Lerp(transform.position.x, targetX, Time.fixedDeltaTime * followSpeed);
+        //float newY = Mathf.Lerp(transform.position.y, targetY, Time.fixedDeltaTime * followSpeed);
+
+        float newX = targetX;
+        float newY = targetY;
+
+        Rect targetRect = new Rect
+        {
+            x = newX - halfViewSizeX,
+            y = newY - halfViewSizeY,
+            width = halfViewSizeX * 2,
+            height = halfViewSizeY * 2
+        };
+
+        if (targetRect.xMin < mapArea.xMin)
+        {
+            targetRect.x = mapArea.xMin;
+        }
+        else if (targetRect.xMax > mapArea.xMax)
+        {
+            targetRect.x = mapArea.xMax - targetRect.width;
+        }
+
+        if (targetRect.yMin < mapArea.yMin)
+        {
+            targetRect.y = mapArea.yMin;
+        }
+        else if (targetRect.yMax > mapArea.yMax)
+        {
+            targetRect.y = mapArea.yMax - targetRect.height;
+        }
+
+        transform.position = new Vector3(targetRect.center.x, targetRect.center.y, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public Tiled2Unity.TiledMap map;
+
+    public float speedFactor = 100.0f;
+
+    private Rigidbody2D rigid;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 vel = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+        //transform.Translate(vel * Time.deltaTime * speedFactor);
+        rigid.velocity = vel * speedFactor;
+    }
+}
