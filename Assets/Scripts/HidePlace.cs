395a93be716f24f35704f5a055436c6f340abadc
zhangpengfei1017 <zhangpengfei1017@gmail.com>
2016-10-12 18:14:35 -0400

    Scene updated

--- Assets/Scripts/HidePlace.cs
+++ Assets/Scripts/HidePlace.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class HidePlace : MonoBehaviour {
     public GameObject Player;
-    public float crimeIndex;
 
 	// Use this for initialization
 	void Start () {
@@ -13,8 +12,6 @@ public class HidePlace : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         if (Player != null) {
-            crimeIndex = Mathf.Clamp(crimeIndex - Time.deltaTime, 0, 100);
-            Player.GetComponent<PlayerController>().CrimeIndex = Mathf.FloorToInt(crimeIndex);
             if (Input.GetButtonDown("GoOut"))
             {               
                 Player.SetActive(true);
@@ -25,7 +22,6 @@ public class HidePlace : MonoBehaviour {
 	}
     public void HidePlayer(GameObject p) {
         Player = p;
-        crimeIndex = p.GetComponent<PlayerController>().CrimeIndex;
         p.SetActive(false);
     }
 }
