38666f4bce634a94098ba195d77d43f9368c6628
Chi Shen <shenchi710@gmail.com>
2016-10-12 21:39:21 -0400

    little girl animation

---
+++ Assets/Scripts/AnimController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimController : MonoBehaviour
+{
+    private Animator anim;
+    private Rigidbody2D rigid;
+
+    private int velXId;
+    private int velYId;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody2D>();
+
+        velXId = Animator.StringToHash("velX");
+        velYId = Animator.StringToHash("velY");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat(velXId, rigid.velocity.x);
+        anim.SetFloat(velYId, rigid.velocity.y);
+    }
+}
---
+++ Assets/Scripts/TestAnimation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestAnimation : MonoBehaviour
+{
+    public Vector2 vel;
+    private Rigidbody2D rigid;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigid.velocity = vel;
+    }
+}
