eaf99ebc534e40c9b81ebcac3474da7d2445c6c3
Chi Shen <shenchi710@gmail.com>
2016-10-12 20:02:18 -0400

    tweak day light

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -1,5 +1,15 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class TimeTrigger
+{
+    public int day;
+    public float hour;
+    public GameObject messageReceiver;
+    public string message;
+}
 
 public class TimeManager : MonoBehaviour
 {
@@ -10,25 +20,66 @@ public class TimeManager : MonoBehaviour
     public Color dawnColor;
     public Color noonColor;
 
-    private float timeWhenDayBegin;
-    
+    public TimeTrigger[] timeTriggers;
+
+
     /// <summary>
     /// 0 for 8:00 am
     /// </summary>
     public float VirtualTimeInHour { get; private set; }
 
+    public int Day
+    {
+        get
+        {
+            return dayPassed + 1;
+        }
+    }
+
+    public List<TimeTrigger> triggers;
+
+    private bool paused = true;
+    private float timeWhenDayBegin;
+    private int dayPassed = 0;
+    
+    void Awake()
+    {
+        triggers = new List<TimeTrigger>(timeTriggers);
+    }
+
     // Use this for initialization
     void Start()
     {
-        timeWhenDayBegin = Time.time;
+        StartDay();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (paused)
+            return;
+
         float elapsedTime = Time.time - timeWhenDayBegin;
         float progress = elapsedTime / timeForDayTime;
         VirtualTimeInHour = progress * 12;
+
+        for (int i = triggers.Count - 1; i >= 0; i--)
+        {
+            var trigger = triggers[i];
+
+            if (Day != trigger.day && trigger.day != -1)
+                continue;
+
+            if (VirtualTimeInHour > trigger.hour)
+            {
+                trigger.messageReceiver.SendMessage(trigger.message);
+            }
+
+            if (trigger.day != -1)
+            {
+                triggers.RemoveAt(i);
+            }
+        }
 
         if (null != clockPointer)
         {
@@ -38,8 +89,27 @@ public class TimeManager : MonoBehaviour
 
         if (null != dayLightEffect)
         {
-            float t = Mathf.Clamp01(Mathf.Pow((Mathf.Cos(progress * 2.0f * Mathf.PI) + 1) * 0.5f, 7) );
+            float x = progress * 0.8f + 0.2f;
+            float t = Mathf.Clamp01(Mathf.Pow((Mathf.Cos(x * 2.0f * Mathf.PI) + 1) * 0.5f, 3) );
             dayLightEffect.tintColor = Color.Lerp(noonColor, dawnColor, t);
         }
+    }
+
+    void StartDay()
+    {
+        if (!paused)
+            return;
+        
+        timeWhenDayBegin = Time.time;
+        paused = false;
+    }
+
+    void EndDay()
+    {
+        if (paused)
+            return;
+
+        paused = true;
+        dayPassed++;
     }
 }
